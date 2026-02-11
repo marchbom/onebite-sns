a39564dfddde9a0a5d5d1e238b74ac86a129535f
Bomin Kim <sirubom1203@gmail.com>
2026-02-11 22:01:24 +0900

    feat like

--- onebite-log/src/components/post/post-item.tsx
+++ onebite-log/src/components/post/post-item.tsx
@@ -5,7 +5,6 @@ import {
   CarouselContent,
   CarouselItem,
 } from "@/components/ui/carousel";
-import type { Post } from "@/type";
 import { formatTimeAgo } from "@/lib/time";
 import EditPostButton from "./edit-post-button";
 import DeletePostButton from "./delete-post-button";
@@ -13,6 +12,7 @@ import { useSession } from "@/store/session";
 import { usePostByIdData } from "@/hooks/queries/use-post-by-id-data";
 import Loader from "../loader";
 import Fallback from "../fallback";
+import LikePostButton from "./like-post-button";
 
 export default function PostItem({ postId }: { postId: number }) {
   const session = useSession();
@@ -90,10 +90,7 @@ export default function PostItem({ postId }: { postId: number }) {
       {/* 3. 좋아요, 댓글 버튼 */}
       <div className="flex gap-2">
         {/* 3-1. 좋아요 버튼 */}
-        <div className="hover:bg-muted flex cursor-pointer items-center gap-2 rounded-xl border-1 p-2 px-4 text-sm">
-          <HeartIcon className="h-4 w-4" />
-          <span>0</span>
-        </div>
+        <LikePostButton id={post.id} likeCount={post.like_count} />
 
         {/* 3-2. 댓글 버튼 */}
         <div className="hover:bg-muted flex cursor-pointer items-center gap-2 rounded-xl border-1 p-2 px-4 text-sm">
